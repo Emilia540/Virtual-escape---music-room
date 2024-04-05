a30eb0f4b515bb4efa69e14d288106668294b7a3
Emilia540 <159914978+Emilia540@users.noreply.github.com>
2024-04-05 11:05:53 +0200

    Endra på ting, fjerna tekst

--- Musikk rom - PHP.php
+++ Musikk rom - PHP.php
@@ -1,7 +1,7 @@
 <?php
 
-$roomTitle = "Klarer du å løse lydkoden i tide?";
-$roomDescription = "Du er en detektiv som prøver å løse en lydkode for å komme ut. Klarer du det før tida løper ut?";
+
+$roomDescription = "Klarer du å løse lydkoden før tida løper ut?";
 
 echo "<!DOCTYPE html>";
 echo "<html>";
@@ -10,7 +10,7 @@ echo "<title>Virtuell Escape Room</title>";
 echo "<link rel='stylesheet' type='text/css' href='Musikk rom - css.css'>";
 echo "</head>";
 echo "<body>";
-echo "<h1>" . $roomTitle . "</h1>";
+echo "<img src='Gitar.png'>";
 echo "<p>" . $roomDescription . "</p>";
 echo "</body>";
 echo "</html>";
