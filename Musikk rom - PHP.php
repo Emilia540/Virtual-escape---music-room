0b5fb8cc37858b021d23012a7a64725f32ebe1fd
Emilia540 <159914978+Emilia540@users.noreply.github.com>
2024-04-05 09:10:39 +0200

    Create Musikk rom - PHP.php
    Starta på nytt

---
+++ Musikk rom - PHP.php
@@ -0,0 +1,17 @@
+<?php
+
+$roomTitle = "Klarer du å løse lydkoden i tide?";
+$roomDescription = "Du er en detektiv som prøver å løse en lydkode for å komme ut. Klarer du det før tida løper ut?";
+
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<head>";
+echo "<title>Virtuell Escape Room</title>";
+echo "<link rel='stylesheet' type='text/css' href='Musikk rom - css.css'>";
+echo "</head>";
+echo "<body>";
+echo "<h1>" . $roomTitle . "</h1>";
+echo "<p>" . $roomDescription . "</p>";
+echo "</body>";
+echo "</html>";
+?>
