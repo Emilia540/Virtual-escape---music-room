d9082e4cc36eb81bf564eb6f2305c9cb4f818bc1
Emilia540 <159914978+Emilia540@users.noreply.github.com>
2024-04-17 08:40:39 +0200

    Add files via upload

--- Musikk rom - PHP.php
+++ Musikk rom - PHP.php
@@ -5,39 +5,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Musikk rommet</title>
     <style>
-        .right-align {
+        body {
+            font-family: 'Courier New', Courier, monospace;
+            background-image: linear-gradient(200deg, #837d88, #7ff0d7);
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .container {
+            max-width: 80%;
+            margin-left: auto;
+            margin-right: 145px;
+            margin-top: -350px; /* Justerer margintoppen for å flytte teksten oppover */
+        }
+
+        .container p {
             text-align: right;
+        }
+
+        img {
+            width: 200px;
+            height: 250px;
+        }
+
+        img {
+            width: 50vw;
+            height: 100vh;
+            object-fit: cover;
+        }
+
+        img {
+            position: absolute;
+            left: 0;
         }
     </style>
 </head>
 <body>
-
-<?php
-    $roomDescription = "Klarer du å gjette hvilken note som åpner døren?";
-    
-    echo "<!DOCTYPE html>";
-    echo "<html>";
-    echo "<head>";
-    echo "<title>Virtuell Escape Room</title>";
-    echo "<link rel='stylesheet' type='text/css' href='Musikk rom - css.css'>";
-    echo "</head>";
-    echo "<body>";
-    echo "<img src='Gitar.png'>";
-    echo "<p>" . $roomDescription . "</p>";
-    echo "</body>";
-    echo "</html>";
-    ?>
-
-
-</body>
-</html>
 <div class="container">
     <?php
         $roomDescription = "Klarer du å gjette hvilken note som åpner døren?";
-       
-        echo "<p class='right-align'>" . $roomDescription . "</p>";
+        echo "<p>" . $roomDescription . "</p>";
     ?>
 </div>
 
+<img src="Gitar.png">
 </body>
 </html>
+
