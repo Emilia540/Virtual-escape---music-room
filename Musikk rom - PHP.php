676615a31250256258d44550b4c784191cb56524
Emilia540 <159914978+Emilia540@users.noreply.github.com>
2024-04-17 08:29:48 +0200

    Add files via upload

--- Musikk rom - PHP.php
+++ Musikk rom - PHP.php
@@ -1,17 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Musikk rommet</title>
+    <style>
+        .right-align {
+            text-align: right;
+        }
+    </style>
+</head>
+<body>
+
 <?php
+    $roomDescription = "Klarer du å gjette hvilken note som åpner døren?";
+    
+    echo "<!DOCTYPE html>";
+    echo "<html>";
+    echo "<head>";
+    echo "<title>Virtuell Escape Room</title>";
+    echo "<link rel='stylesheet' type='text/css' href='Musikk rom - css.css'>";
+    echo "</head>";
+    echo "<body>";
+    echo "<img src='Gitar.png'>";
+    echo "<p>" . $roomDescription . "</p>";
+    echo "</body>";
+    echo "</html>";
+    ?>
 
 
-$roomDescription = "Klarer du å løse lydkoden før tida løper ut?";
+</body>
+</html>
+<div class="container">
+    <?php
+        $roomDescription = "Klarer du å gjette hvilken note som åpner døren?";
+       
+        echo "<p class='right-align'>" . $roomDescription . "</p>";
+    ?>
+</div>
 
-echo "<!DOCTYPE html>";
-echo "<html>";
-echo "<head>";
-echo "<title>Virtuell Escape Room</title>";
-echo "<link rel='stylesheet' type='text/css' href='Musikk rom - css.css'>";
-echo "</head>";
-echo "<body>";
-echo "<img src='Gitar.png'>";
-echo "<p>" . $roomDescription . "</p>";
-echo "</body>";
-echo "</html>";
-?>
+</body>
+</html>
